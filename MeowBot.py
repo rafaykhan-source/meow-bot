df5b8487c976fc6130cc0ed404267caab4e08884
Rafay Khan <82721722+rafaykhan-source@users.noreply.github.com>
2022-08-03 20:35:34 -0400

    commiting meowbot version 1

---
+++ MeowBot.py
@@ -0,0 +1,36 @@
+import os
+import discord
+import time
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+  print(f'{client.user} is online!')
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+
+  if message.content.startswith('!meowtimer'):
+    input = message.content.split()
+    await message.channel.send("meow timer starting now!")
+    try:
+      time.sleep(float(input[1]))
+      await message.channel.send("meow!")
+    except:
+      time.sleep(float(3))
+      await message.channel.send("meow!")
+    return
+      
+  if message.content.startswith('!meow'):
+    input = message.content.split()
+    amt = int(input[1])
+    if amt <= 50 and amt > 0:
+      await message.channel.send("meow"*amt)
+    else:
+      await message.channel.send("too many meows... shutting down")
+    return
+
+client.run(os.environ['meow'])
